001bb3a86249ded59c4815965f1af86a3c2743e5
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2024-12-06 11:48:45 -0300

    comitado

---
+++ RR04/RR4-01-Roteiro-Revisao-Recursao-environment/src/main/java/recursao/TestarMetodosRecursivos.java
@@ -0,0 +1,11 @@
+package recursao;
+
+public class TestarMetodosRecursivos {
+	public static void main(String[] args) {
+		
+		MetodosRecursivos metR = new MetodosRecursivos();
+
+		//metR.calcularFatorial(6);
+		metR.calcularFibonacci(4);
+	}
+}
