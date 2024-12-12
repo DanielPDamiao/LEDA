0d6e58163f8ea787afbef041067cd850d2607279
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2024-12-12 20:20:34 -0300

    quick feito

--- IMPLEMENTACOES/src/QuickSort.java
+++ IMPLEMENTACOES/src/QuickSort.java
@@ -1,31 +1,35 @@
 public class QuickSort {
     
     public void QuiSort(int[] array, int ini, int fim){
-        if(ini == fim){
+        if(ini >= fim){
             return;
         }
-        int i = partition(array, ini, fim, ini, ini+1);
+        int i = partition(array, ini, fim);
         QuiSort(array, ini, i-1);
         QuiSort(array, i+1, fim);
     }
 
-    private int partition(int[] array, int ini, int fim, int i, int j){
+    private int partition(int[] array, int ini, int fim){
         int pivot = ini;
-        if(j > fim){
-            int troca = array[i];
-            array[i] = array[pivot];
-            array[pivot] = troca;
+        int i = ord(array, ini, ini+1, pivot);
+        int troca = array[i];
+        array[i] = array[pivot];
+        array[pivot] = troca;
+        return i;
+    }
+
+    private int ord(int[] array, int i, int j, int pivot){
+        if(j == array.length){
+            return i;
         }
-        else if(array[j] >= array[pivot]){
-            i = partition(array, ini, fim, i, j+1);
-        }
-        else{
-            i = i+1;
+        if(array[j] < array[pivot]){
+            i += 1;
             int troca = array[i];
             array[i] = array[j];
             array[j] = troca;
-            i = partition(array, ini, fim, i, j+1);
+            
         }
+        i = ord(array, i, j+1, pivot);
         return i;
     }
 }
