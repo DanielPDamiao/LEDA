2316f40f9f47470d627c8b209b76d52c01749832
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2025-02-05 09:56:00 -0300

    seloko

---
+++ R04-01-Rot-LinearSorting-Countingsort-environment/src/main/java/sorting/linearSorting/CountingSort.java
@@ -0,0 +1,56 @@
+package sorting.linearSorting;
+
+import sorting.AbstractSorting;
+
+/**
+ * Classe que implementa a estratégia de Counting Sort vista em sala.
+ *
+ * Procure evitar desperdício de memória: AO INVÉS de alocar o array de contadores
+ * com um tamanho arbitrariamente grande (por exemplo, com o maior valor de entrada possível),
+ * aloque este array com o tamanho sendo o máximo inteiro presente no array a ser ordenado.
+ *
+ * Seu algoritmo deve assumir que o array de entrada nao possui numeros negativos,
+ * ou seja, possui apenas numeros inteiros positivos e o zero.
+ *
+ */
+public class CountingSort extends AbstractSorting<Integer> {
+
+	@Override
+	public void sort(Integer[] array, int leftIndex, int rightIndex) {
+		if(rightIndex < leftIndex){
+			return;
+		}
+
+		int maxValue = findMax(array, leftIndex, rightIndex);
+		int[] countArray = new int[maxValue+1];
+		
+		for(int i = leftIndex; i <= rightIndex; i++){
+			countArray[array[i]] += 1;
+		}
+
+		for(int i = 1; i < countArray.length; i++){
+			countArray[i] += countArray[i-1];
+		}
+
+		int[] compArray = new int[rightIndex-leftIndex+1];
+		for(int i = leftIndex; i <= rightIndex; i++){
+			compArray[i] = array[i];
+		}
+
+		for(int i = leftIndex; i <= rightIndex; i++){
+			array[countArray[compArray[i]]-1] = compArray[i];
+			countArray[compArray[i]] -= 1;
+		}
+	}
+
+	private int findMax(Integer[] array, int leftIndex, int rightIndex) {
+		int maxValue = array[leftIndex];
+		for(int i = leftIndex+1; i <= rightIndex; i++){
+			if(array[i] > maxValue){
+				maxValue = array[i];
+			}
+		}
+		return maxValue;
+	}
+
+}
