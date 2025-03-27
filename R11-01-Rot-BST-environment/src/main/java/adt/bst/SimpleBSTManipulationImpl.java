3f66ca74fc06c104751eca0920c889fe727086ad
DanielPDamiao <daniel.pereira.damiao@ccc.ufcg.edu.br>
2025-03-27 17:23:07 -0300

    hmm

--- R11-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
+++ R11-01-Rot-BST-environment/src/main/java/adt/bst/SimpleBSTManipulationImpl.java
@@ -10,20 +10,75 @@ public class SimpleBSTManipulationImpl<T extends Comparable<T>> implements Simpl
 
 	@Override
 	public boolean equals(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return equals((BSTNode<T>)tree1.getRoot(), (BSTNode<T>)tree2.getRoot());
+	}
+
+	private boolean equals(BSTNode<T> node, BSTNode<T> node2){
+		boolean resp = true;
+		if(!node.isEmpty() || !node2.isEmpty()){
+			if(node.isEmpty() || node2.isEmpty()){
+				resp = false;
+			}
+			else if(!node.getData().equals(node2.getData())){
+				resp = false;
+			}
+			else{
+				resp = equals((BSTNode<T>)node.getLeft(), (BSTNode<T>)node2.getLeft()) && equals((BSTNode<T>)node.getRight(), (BSTNode<T>)node2.getRight());
+			}
+		}
+		return resp;
 	}
 
 	@Override
 	public boolean isSimilar(BST<T> tree1, BST<T> tree2) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return isSimilar((BSTNode<T>)tree1.getRoot(), (BSTNode<T>)tree2.getRoot());
+	}
+
+	private boolean isSimilar(BSTNode<T> node, BSTNode<T> node2){
+		boolean resp = true;
+		if(!node.isEmpty() || !node2.isEmpty()){
+			if(node.isEmpty() || node2.isEmpty()){
+				resp = false;
+			}
+			else{
+				resp = isSimilar((BSTNode<T>)node.getLeft(), (BSTNode<T>)node2.getLeft()) && isSimilar((BSTNode<T>)node.getRight(), (BSTNode<T>)node2.getRight());
+			}
+		}
+		return resp;
 	}
 
 	@Override
 	public T orderStatistic(BST<T> tree, int k) {
-		// TODO Implement this method
-		throw new UnsupportedOperationException("Not implemented yet!");
+		return orderStatistic(tree, (BSTNode<T>)tree.getRoot(), k);
+	}
+
+	private T orderStatistic(BST<T> tree, BSTNode<T> node, int k){
+		T resp = null;
+		if(!node.isEmpty()){
+			if(k == 1){
+				resp = node.getData();
+			}
+			else{
+				resp = orderStatistic(tree, sucessorOf((BSTNode<T>)tree.getRoot(), node.getData()), k-1);
+			}
+		}
+		return resp;
+	}
+
+	private BSTNode<T> sucessorOf(BSTNode<T> node, T element){
+		BSTNode<T> resp = null;
+		if(!node.isEmpty()){
+			if(node.getData().compareTo(element) > 0){
+				resp = sucessorOf((BSTNode<T>)node.getLeft(), element);
+				if(resp == null){
+					resp = node;
+				}
+			}
+			else{
+				resp = sucessorOf((BSTNode<T>)node.getRight(), element);
+			}
+		}
+		return resp;
 	}
 
 }
